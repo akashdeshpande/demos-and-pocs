4d2f54733ab6d2aaf296a0ee198f19c14a15bd52
Akash Deshpande <33683931+akashdeshpande@users.noreply.github.com>
2022-06-02 11:24:59 +0530

    Added function to every button in the table

--- TableModal/assets/js/script.js
+++ TableModal/assets/js/script.js
@@ -1,7 +1,10 @@
+let receivedData = {};
+
 $("#btn1").click(function(){
     $.ajax({url: "https://jsonplaceholder.typicode.com/todos", success: function(result){
     //   $("#div1").html(result);
         console.log(result);
+        receivedData = result;
         let child = "";
         for(let i = 0; i < result.length; i++) {
             child = "";
@@ -11,14 +14,13 @@ $("#btn1").click(function(){
                 <td>${userId}</td>
                 <td>${title}</td>
                 <td>${completed}</td>
-                <td><button class="btn btn-outline-light edit-btn">Edit</button></td>
+                <td><button class="btn btn-outline-light edit-btn" onlick="editData(${id})">Edit</button></td>
             </tr>`;
             $("#table1").append(child);
         }
-        
     }});
 });
 
 function editData(id) {
-    console.log(id);
+    console.log(receivedData[id]);
 } 
