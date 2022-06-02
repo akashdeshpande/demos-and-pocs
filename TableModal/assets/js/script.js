0ea69b8735b44eefce090e969f5b7008a2ab2fc5
Akash Deshpande <33683931+akashdeshpande@users.noreply.github.com>
2022-06-02 11:06:31 +0530

    Added script function to append children to table

--- TableModal/assets/js/script.js
+++ TableModal/assets/js/script.js
@@ -2,5 +2,19 @@ $("#btn1").click(function(){
     $.ajax({url: "https://jsonplaceholder.typicode.com/todos", success: function(result){
     //   $("#div1").html(result);
         console.log(result);
+        let child = "";
+        for(let i = 0; i < result.length; i++) {
+            child = "";
+            let { id, userId, title, completed } = result[i];
+            child = `<tr>
+                <td>${id}</td>
+                <td>${userId}</td>
+                <td>${title}</td>
+                <td>${completed}</td>
+                <td><button class="btn btn-outline-light">Edit</button></td>
+            </tr>`;
+            $("#table1").append(child);
+        }
+        
     }});
 });
